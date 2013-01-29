9a59fb1aca35975399fa958ca1e38ffcbca53b20
ollietb <me@oll.ie>
2013-01-29 09:13:35 +0000

    deleting unused files

--- DependencyInjection/OhColorPickerTypeExtension.php
+++ DependencyInjection/OhColorPickerTypeExtension.php
@@ -22,7 +22,5 @@ class OhColorPickerTypeExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
     }
 }
