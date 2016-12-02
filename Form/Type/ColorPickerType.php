4fe3081756cb760c3a1b3c9643cbc9bbc382c31e
Vadim Shurman <vadym.shurman@ekreative.com>
2016-12-02 10:49:16 +0200

    refactoring fo Symfony 2.8

--- Form/Type/ColorPickerType.php
+++ Form/Type/ColorPickerType.php
@@ -11,51 +11,65 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 class ColorPickerType extends AbstractType
 {
 
-/**
- * {@inheritdoc}
- */
-public function configureOptions(OptionsResolver $resolver)
-{
-        $choiceList = function (Options $options) use (&$choiceListCache) {
-            $choices = array(
-                '#ffffff' => 'White',
-                '#cccccc' => 'Light Grey',
-                '#777777' => 'Grey',
-                '#333333' => 'Dark Grey',
-                '#000000' => 'Black',
-                '#2f4096' => 'Dark Blue',
-                '#0082be' => 'Light Blue',
-                '#32c4ec' => 'Aqua',
-                '#00944f' => 'Dark Green',
-                '#39af4c' => 'Light Green',
-                '#b2ce40' => 'Greenish',
-                '#6c2c8e' => 'Dark Purple',
-                '#913990' => 'Purple',
-                '#b989ba' => 'Lilac',
-                '#F934C5' => 'Dark Pink',
-                '#f3aebc' => 'Light Pink',
-                '#e81e25' => 'Red',
-                '#ed7043' => 'Peach',
-                '#f39f2b' => 'Orange',
-                '#ecba4b' => 'Light Orange',
-                '#fce92d' => 'Yellow',
-                '#f9eea9' => 'Cream',
-            );
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
 
-            $resolver->setDefaults(array(
-                'multiple' => false,
-                'expanded' => false,
-                'choices' => $choices,
-                'labels' => $choices,
-                'choices_as_values' => false,
-                'include_jquery' => false,
-                'include_js' => false,
-                'include_js_constructor' => true,
-                'include_css' => false,
-                'picker' => false,
-            ));
+        $choices = array(
+            '#ffffff' => 'White',
+            '#cccccc' => 'Light Grey',
+            '#777777' => 'Grey',
+            '#333333' => 'Dark Grey',
+            '#000000' => 'Black',
+            '#2f4096' => 'Dark Blue',
+            '#0082be' => 'Light Blue',
+            '#32c4ec' => 'Aqua',
+            '#00944f' => 'Dark Green',
+            '#39af4c' => 'Light Green',
+            '#b2ce40' => 'Greenish',
+            '#6c2c8e' => 'Dark Purple',
+            '#913990' => 'Purple',
+            '#b989ba' => 'Lilac',
+            '#F934C5' => 'Dark Pink',
+            '#f3aebc' => 'Light Pink',
+            '#e81e25' => 'Red',
+            '#ed7043' => 'Peach',
+            '#f39f2b' => 'Orange',
+            '#ecba4b' => 'Light Orange',
+            '#fce92d' => 'Yellow',
+            '#f9eea9' => 'Cream',
+        );
 
-        }
+        $resolver->setDefaults([
+            'multiple' => false,
+            'expanded' => false,
+            'choices' => $choices,
+            'labels' => $choices,
+            'choices_as_values' => false,
+            'include_jquery' => true,
+            'include_js' => true,
+            'include_js_constructor' => true,
+            'include_css' => false,
+            'picker' => false,
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function buildView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::buildView($view, $form, $options);
+
+        $view->vars['include_jquery'] = $options['include_jquery'];
+        $view->vars['include_js'] = $options['include_js'];
+        $view->vars['include_js_constructor'] = $options['include_js_constructor'];
+        $view->vars['include_css'] = $options['include_css'];
+        $view->vars['picker'] = $options['picker'];
+
+    }
 
     /**
      * {@inheritdoc}
